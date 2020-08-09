c4304ba8c66074c6528eaa5a086552e51862fc6a
ygoldfrid <ygoldfrid@gmail.com>
2020-08-09 16:35:43 -0500

    Profile layout fix

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -12,31 +12,33 @@ function Profile() {
   if (!user) return <Redirect to="/" />;
 
   return (
-    <div className="mt-5">
-      {user && (
-        <div className="profile mb-5">
-          <div className="card text-center">
-            <div className="card-header">My Profile</div>
-            <div className="card-body">
-              <h5 className="card-title">{user.name}</h5>
-              <p className="card-text mb-2">{user.email}</p>
-              <button className="link" onClick={auth.logout}>
-                Sign out
-              </button>
+    <>
+      <div className="profile-container mt-5">
+        {user && (
+          <div className="profile mb-5">
+            <div className="card text-center">
+              <div className="card-header">My Profile</div>
+              <div className="card-body">
+                <h5 className="card-title">{user.name}</h5>
+                <p className="card-text mb-2">{user.email}</p>
+                <button className="link" onClick={auth.logout}>
+                  Sign out
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      )}
-      {favorites && favorites.length > 0 && (
-        <div className="favorites-container">
-          <div className="row mb-3 justify-content-center">
-            <h4 className="text-center">My Favorite Cocktails</h4>
+        )}
+        {favorites && favorites.length > 0 && (
+          <div className="favorites-container">
+            <div className="row mb-3 justify-content-center">
+              <h4 className="text-center">My Favorite Cocktails</h4>
+            </div>
+            <CocktailList cocktails={favorites} size="big" />
           </div>
-          <CocktailList cocktails={favorites} size="big" />
-        </div>
-      )}
+        )}
+      </div>
       <Footer />
-    </div>
+    </>
   );
 }
 
