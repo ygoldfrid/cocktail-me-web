809e153b2e1045f503be3eda379200e2f357fe08
Yaniv Goldfrid <ygoldfrid@gmail.com>
2022-12-28 18:35:22 +0200

    fixing image path

--- src/components/common/Image.jsx
+++ src/components/common/Image.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import ProgressiveImage from "react-progressive-image";
-import { imagePath } from "../../utils/imagePath";
 
 function Image({ images, alt, ...rest }) {
   return (
     <ProgressiveImage
-      src={imagePath(images[0].url)}
-      placeholder={imagePath(images[0].thumbnailUrl)}
+      src={images[0].url}
+      placeholder={images[0].thumbnailUrl}
     >
       {(src) => <img src={src} alt={alt} {...rest} />}
     </ProgressiveImage>
